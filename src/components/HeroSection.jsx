f546db199e16ebcb6f622ff583e5ed29a6d79e5a
Daniels-124 <danielemeni06@gmail.com>
2025-05-26 22:36:45 +0100

    My first commit.

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+function HeroSection() {
+  return (
+    <div className="hero-section">
+      <h1 className="hero-title">Welcome to VIN PAY Africa</h1>
+      <p className="hero-subtitle">
+        Your smart way to send, receive, and manage funds.
+      </p>
+      <button className="hero-button">Get Started</button>
+    </div>
+  );
+}
+
+export default HeroSection
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import './components/styles/Navbar.css'
+import './components/styles/HeroSection.css'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
